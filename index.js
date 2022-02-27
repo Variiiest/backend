f638996b8ec439ad2ae2f29f5aacd3e6daa1d210
Varish Gautam <varishgautamvari@gmail.com>
2022-02-27 13:19:10 +0530

    first commit

--- index.js
+++ index.js
@@ -1,71 +1,46 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-app.use(express.json())
 const cors = require('cors')
-app.use(cors())
-
+require('dotenv').config()
 const Note = require('./models/note')
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-  
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
+app.use(express.json())
 
+app.use(requestLogger)
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+app.use(cors())
 
-  const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+app.use(express.static('build'))
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-
-
-
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
-
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
+  note.save().then(savedNote => {
+    response.json(savedNote)
   })
+})
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
@@ -73,30 +48,25 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
 
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
+  response.status(204).end()
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
   })
+})
 
-
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
--- models/note.js
+++ models/note.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose')
 
-const url= process.env.MONGOOB_URI
-console.log('connecting to', url)
+const url = process.env.MONGODB_URI
 
-mongoose.connect(url).then(
-    result=>{
-        console.log('connected to MongoDB')
-    }
-)
-.catch((error)=>{
-    console.log('error connecting to MongoDB', error.message)
-})
+console.log('connecting to', url)
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
-    content: String, 
-    date: Date, 
-    important: Boolean,
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {
-    transform:(document, returnedObject)=>{
-        returnedObject.id= returnedObject._id.toString()
-        delete returnedObject._id 
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
-module.exports= mongoose.model('Note', noteSchema)
+
+module.exports = mongoose.model('Note', noteSchema)
